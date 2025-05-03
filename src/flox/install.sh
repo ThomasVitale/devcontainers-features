a51fb05c0dbfcc5ffc4fc64a25868c814178f34c
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-05-03 19:20:42 +0200

    Update

--- src/flox/install.sh
+++ src/flox/install.sh
@@ -3,13 +3,14 @@ set -e
 
 echo "Installing Flox"
 
-RUN echo 'extra-trusted-substituters = https://cache.flox.dev' | sudo tee -a /etc/nix/nix.conf && \
-    echo 'extra-trusted-public-keys = flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=' | sudo tee -a /etc/nix/nix.conf
+echo 'extra-trusted-substituters = https://cache.flox.dev' | sudo tee -a /etc/nix/nix.conf
+echo 'extra-trusted-public-keys = flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=' | sudo tee -a /etc/nix/nix.conf
 
-nix profile install github:flox/flox \
-    --extra-experimental-features flakes \
-    --extra-experimental-features nix-command \
-    --accept-flake-config
+sudo -H nix profile install \
+    --profile /nix/var/nix/profiles/default \
+    --experimental-features "nix-command flakes" \
+    --accept-flake-config \
+    'github:flox/flox'
 
 flox --version
 
