1fdef545961d56aeb10e10e36a1d44a95f07bcd7
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-05-03 19:07:57 +0200

    Update tests

--- test/flox/test.sh
+++ test/flox/test.sh
@@ -40,7 +40,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
-check "check flox version reports a version" bash -c "flox version"
+check "check flox version" bash -c "flox --version"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
