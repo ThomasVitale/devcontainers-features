dbe8375f49e61267f499ee1e9d4fb3448596fb7e
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-05-04 11:49:48 +0200

    Update

--- src/flox/install.sh
+++ src/flox/install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable
-nix-channel --update
-
 nix profile install \
-      --extra-experimental-features flakes \
-      --extra-experimental-features nix-command \
-      --accept-flake-config \
-      'github:flox/flox'
+    --extra-experimental-features flakes \
+    --extra-experimental-features nix-command \
+    --accept-flake-config \
+    'github:flox/flox'
